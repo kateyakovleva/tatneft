88a5b7b5e617486c43f7678eeae1c0ec1172e59c
Tercius <mr.terks@yandex.ru>
2025-01-14 17:05:28 +0300

    update

--- frontend/src/app/pages/home/components/about-project/about-project.component.ts
+++ frontend/src/app/pages/home/components/about-project/about-project.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 
-@Component({
+@Component( {
   selector: 'app-about-project',
   standalone: true,
   imports: [],
   templateUrl: './about-project.component.html',
   styleUrl: './about-project.component.scss'
-})
+} )
 export class AboutProjectComponent {
-
+  about = {
+    text1: '«75 лет создаём добро» — масштабный проект Татнефть, посвящённый юбилею Компании. Он объединяет тех, кто ежедневно совершает добрые дела, большие и малые, меняя мир к лучшему.',
+    text2: 'Присоединяйтесь к проекту, станьте частью большого движения — давайте вместе развивать регион!'
+  }
 }
